153cc924fcf67a8160fc96868fb5a3259efc78dc
Jason Antwi-Appah <hey@jasonaa.me>
2025-05-15 21:42:48 -0500

    Update comment/naming in getStack fn

--- src/server/simulator.ts
+++ src/server/simulator.ts
@@ -18,7 +18,7 @@ const srcDir = path.resolve(__dirname, "../");
 
 /**
  * get the current error stack
- * @param justMyCode - restricts the scope of the stack to just the code in the chessBot project. ex: if there is an error thrown in a file in the node_modules folder, this will not be included in the stack.
+ * @param justMyCode - restricts the scope of the stack to just the code in the chessbots-server project. Example: if there is an error thrown in a file in the node_modules folder, this will not be included in the stack.
  * @returns - the stack of the error
  */
 function getStack(justMyCode = true) {
@@ -31,11 +31,11 @@ function getStack(justMyCode = true) {
     const cleanedStack = parseErrorStack(stack);
 
     if (justMyCode) {
-        const chessBotCodeEndFrame = cleanedStack.findIndex(
+        const chessBotsCodeEndFrame = cleanedStack.findIndex(
             (frame) => !frame.fileName.startsWith(srcDir),
         );
-        if (chessBotCodeEndFrame !== -1) {
-            cleanedStack.splice(chessBotCodeEndFrame);
+        if (chessBotsCodeEndFrame !== -1) {
+            cleanedStack.splice(chessBotsCodeEndFrame);
         }
     }
 
