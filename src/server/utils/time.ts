b365918606639560f49fc630cc5599654cba81a5
Dylan Brose <83487906+xXDMOGXx@users.noreply.github.com>
2025-09-16 23:20:38 -0500

    Heartbeat server (#260)
    * CHESSBOTS BEGINS NOW!!!! 🗣️🗣️🗣️🔥🔥🔥
    
    * issue should be done less go
    
    * fixed es lint and various errors
    
    * ok now it should actually be fixed
    
    ---------
    
    Co-authored-by: MahdMalik <mahdmahdmalik@gmail.com>

---
+++ src/server/utils/time.ts
@@ -0,0 +1,5 @@
+//waits for a certain amount of time, as specified by the parameter.
+//Call await waitTime(timeInMs) to do it. Returns a promise that will resolve after a set amount of time
+export async function waitTime(msTime: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, msTime));
+}
