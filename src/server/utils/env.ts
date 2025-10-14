540cb23c80a391642e2dc7b472ace42c665a0f8b
MahdMalik <mahdmahdmalik@gmail.com>
2025-10-13 21:05:49 -0500

    not sure why this is failing on the main branch

--- src/server/utils/env.ts
+++ src/server/utils/env.ts
@@ -6,8 +6,7 @@ export const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 export const IS_PRODUCTION = !IS_DEVELOPMENT;
 export const DO_SAVES = process.env.ENABLE_SAVES === "true";
 export const USE_VIRTUAL_ROBOTS = process.env.VIRTUAL_ROBOTS === "true";
-export const START_ROBOTS_AT_DEFAULT =
-    process.env.START_ROBOTS_AT_DEFAULT === "true";
+export const START_ROBOTS_AT_DEFAULT = process.env.START_ROBOTS_AT_DEFAULT;
 
 export const PING_INTERVAL = 1000;
 export const PING_TIMEOUT = 100;
